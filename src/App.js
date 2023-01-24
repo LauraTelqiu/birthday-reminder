a470af53462ebd039099bafa79921ecd7ef4414b
Laura <lauratelqiu@gmail.com>
2023-01-24 11:34:17 -0500

    list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import { useState } from "react";
+import data from "./Data.js";
+import List from "./List";
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
+    <main>
+      <section className="contanier">
+        <h3>{people.length} Birthdays Today</h3>
 
-    <div className='container'>
-      Birthday
-    </div>
+        <List people={people} />
+
+        <button onClick={() => console.log("cliked")}>Clear All</button>
+      </section>
+    </main>
   );
 }
 
