cd759b52b028cf8494a9de6e24b685605f3865ef
Laura <lauratelqiu@gmail.com>
2023-01-29 10:11:37 -0500

    review

--- reviews/src/App.js
+++ reviews/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Review from './Review';
-import { FaGithubSquare } from 'react-icons/fa';
+
 
 function App() {
-  return <h2>
-    <FaGithubSquare />
-  </h2>;
+  return (
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+
+        </div>
+        <Review />
+      </section>
+    </main>
+  )
 }
 
 export default App;
