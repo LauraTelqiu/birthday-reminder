e21ef393f45e6abc9e7e0db24844818784d991e8
Laura <lauratelqiu@gmail.com>
2023-04-24 21:26:33 -0400

    author

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,44 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
+// import Posts from './Posts'
+import Authors from './Authors'
 
-
-function App() {
-  const [postData, setPostData] = useState([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const jsonData = await response.json();
-        setPostData(jsonData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchPosts();
-  }, []);
-
+const App = () => {
   return (
-    <main>
-      <h1>Posts</h1>
-      <p>Below this tag, lets render some posts</p>
-      <div id="posts" className="posts">
-        <ul>
-          {postData.map((post) => {
-            return (
-
-              <li key={post.id}>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-              </li>
-            )
-
-
-          })}
-        </ul>
-      </div>
-    </main>
-
-  );
+    <div>
+      {/* <Posts /> */}
+      <Authors />
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ practice/src/Authors.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Authors = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Authors
