f10a31947b7623c5793b4f3a68ed473b426b64d7
Laura <lauratelqiu@gmail.com>
2023-04-25 23:03:14 -0400

    content

--- practice/src/Course.js
+++ practice/src/Course.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
+import { sections } from "./data";
 
 const Course = () => {
   return (
-    <>
-      <div className='flex flex-row'>
-        <h1>Getting Started</h1>
-
+    <div>
+      <div className="flex justify-between flex-col m-10 border black ">
+        <div >
+          {sections.map((section) => {
+            return (
+              <div className="flex justify-between p-10">
+                <div className="text-left">{section.name}</div>
+                <div className="text-right">
+                  <h2>
+                    {section.lectures} lectures • {section.totalMins} min
+                  </h2>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Course
-
+export default Course;
--- practice/src/data.js
+++ practice/src/data.js
@@ -1,56 +1,67 @@
-const sections = [
+export const sections = [
   {
+    id: 1,
     name: "Getting Started",
     lectures: 10,
     totalMins: 41,
     lessons: [
       {
+        id: 2,
         name: "Welcome To The Course!",
         type: "Preview",
         length: 64,
       },
       {
+        id: 3,
         name: "What is React.js",
         type: "Preview",
         length: 275,
       },
       {
+        id: 4,
         name: "Why React Instead of 'Just Javascript'?",
         type: "Preview",
         length: 445,
       },
       {
+        id: 5,
         name: "Building Single-Page Application(SPAs) with React",
         type: "Preview",
         length: 93,
       },
 
       {
+        id: 6,
         name: "Exploring React.js Alternatives(Angular/Vue)",
         type: "Preview",
         length: 213,
       },
       {
+        id: 7,
         name: "Join our Online Community",
         type: "Preview",
         length: 25,
       },
       {
+        id: 8,
         name: "About This Course & Course Outline",
         type: "Preview",
         length: 507,
       },
       {
+        id: 9,
         name: "The Two Ways (Paths) of Taking this Course",
         type: "Preview",
         length: 262,
       },
       {
+        id: 10,
         name: "Getting The Most Out of This Course",
         type: "Preview",
         length: 266,
       },
       {
+        id: 11,
         name: "Setting Up The Course Dev Environment(Code Editor)",
         type: "Preview",
         length: 295,
